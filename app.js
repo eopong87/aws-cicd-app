26b702e1e70c0f445146cd65b3b147f52c82a374
Nadine Opong <nay@Nadines-MacBook-Pro.local>
2026-04-01 00:03:13 -0400

    Updated UI to HTML

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(`Hello from ${require('os').hostname()}\n`);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  res.end(`
+    <html>
+      <head>
+        <title>Eddie AWS App</title>
+      </head>
+      <body style="background-color:black; color:white; text-align:center; margin-top:100px;">
+        <h1>🚀 Eddie's AWS App</h1>
+        <h2>Running on EC2</h2>
+        <p>Server: ${require('os').hostname()}</p>
+      </body>
+    </html>
+  `);
 });
 
 server.listen(80, () => {
   console.log('Server running on port 80');
 });
-
